29e9f9f9dd358503f932cdb9812d74510ad3e791
Chris Horner <christopher.horner@anz.com>
2020-09-08 16:23:59 +1000

    Update fishy variables

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,8 @@
 if test (uname -s) = "Darwin"
   set -gx PATH ~/Library/Android/sdk/platform-tools/ $PATH
+  set -gx PATH /Applications/Android Studio.app/Contents/jre/jdk/Contents/Home/bin/ $PATH
+  set -x JAVA_HOME JAVA_HOME=/Applications/Android\ Studio.app/Contents/jre/jdk/Contents/Home/
+  alias smackafee='sudo launchctl unload /Library/LaunchDaemons/com.mcafee.*'
 end
 
 alias gw "./gradlew"
