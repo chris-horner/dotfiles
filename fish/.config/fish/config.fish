d5198ff8fd09fb74374fef9d5f663c3ad8253c28
Chris Horner <chorner@squareup.com>
2021-07-06 19:54:12 +1000

    Update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,9 @@
 if test (uname -s) = "Darwin"
   set -gx PATH ~/Library/Android/sdk/platform-tools/ $PATH
-  set -gx PATH /Applications/Android Studio.app/Contents/jre/jdk/Contents/Home/bin/ $PATH
-  set -x JAVA_HOME JAVA_HOME=/Applications/Android\ Studio.app/Contents/jre/jdk/Contents/Home/
-  alias smackafee='sudo launchctl unload /Library/LaunchDaemons/com.mcafee.*'
+  set -x JAVA_HOME (/usr/libexec/java_home -v 11)
+  set -x ANDROID_SDK_ROOT ~/Library/Android/sdk
 end
 
 alias gw "./gradlew"
 alias gitBranches "git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
