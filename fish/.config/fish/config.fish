2657633672aa0e0a69c1b837ecacf8ffb7abf62b
Chris Horner <chorner@squareup.com>
2024-02-06 11:36:29 +1100

    Update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,12 @@
 if test (uname -s) = "Darwin"
   set -gx PATH ~/Library/Android/sdk/platform-tools/ $PATH
-  set -x JAVA_HOME (/usr/libexec/java_home -v 11)
+  set -x JAVA_HOME (/usr/libexec/java_home -v 17)
   set -x ANDROID_SDK_ROOT ~/Library/Android/sdk
+  set -x SECRETS_PATH ~/secrets
+  source ~/secrets/config.fish
 end
 
 alias gw "./gradlew"
 alias gitBranches "git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+git machete completion fish | source
